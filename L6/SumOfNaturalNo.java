d016112fea4965e3a6a8f7bfb3967b3daccb3a60
shreya kumari <104058567+shreypilot@users.noreply.github.com>
2023-06-06 05:36:29 +0530

    add l-06-loops

---
+++ L6/SumOfNaturalNo.java
@@ -0,0 +1,16 @@
+package L6;
+
+public class SumOfNaturalNo {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        int  n = 4;
+
+        for(int  i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
