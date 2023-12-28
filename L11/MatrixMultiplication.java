84a7a2cb98445d23533deeb76edef9464dd16263
shreypilot <shreypilot28@gmail.com>
2023-12-29 01:41:33 +0530

    matrix-multiplication

---
+++ L11/MatrixMultiplication.java
@@ -0,0 +1,47 @@
+package L11;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int rows1 = matrix1.length;
+        int columns1 = matrix1[0].length;
+        int rows2 = matrix2.length;
+        int columns2 = matrix2[0].length;
+
+        if (columns1 != rows2) {
+            System.out.println("Matrix multiplication is not possible.");
+            return;
+        }
+
+        int[][] result = new int[rows1][columns2];
+
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns2; j++) {
+                for (int k = 0; k < columns1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        // Displaying the result
+        System.out.println("Result of matrix multiplication:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
