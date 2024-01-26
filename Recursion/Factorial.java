d230ac04717e7144c78b33a858c729c6695f1bee
shreypilot <shreypilot28@gmail.com>
2024-01-27 01:41:37 +0530

    Recursion L1-L2

---
+++ Recursion/Factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Factorial {
+    public static int f(int n) {
+        // base case 
+        if(n == 1) return 1;
+        return n * f(n-1); // function works correctly for n-1
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f(4));
+    }
+}
---
+++ Recursion/fibonacci.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class fibonacci {
+    public static int f(int n) {
+        if(n == 0 || n == 1) return n;
+        return f(n-1) + f(n-2);
+    }
+            
+    public static void main(String[] args) {
+        System.out.println(f(5));
+    }
+}
